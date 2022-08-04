5f2ae18ec153793457c5385c2370e8462a7f2a1b
Matthew Crossman <matt@crossman.page>
2022-08-04 17:58:19 +1000

    frontend: Revamp global CSS with new files, variables, etc.

--- source/frontend/pages/_app.tsx
+++ source/frontend/pages/_app.tsx
@@ -1,4 +1,8 @@
-import '../styles/globals.css'
+import '../styles/colours.css'
+import '../styles/values.css'
+import '../styles/variables.css'
+import '../styles/fonts.css'
+import '../styles/base.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
