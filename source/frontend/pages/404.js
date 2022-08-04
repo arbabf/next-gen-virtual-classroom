e3e3466f8b34e21b7bbbc70f6360d587d5d07032
Matthew Crossman <matt@crossman.page>
2022-08-04 18:18:56 +1000

    frontend: change index to component test, enhance 404

--- source/frontend/pages/404.js
+++
@@ -1,6 +0,0 @@
-export default function Custom404() {
-  return (
-	<h1>404 - Page Not Found</h1>
-	<h2>Contact the developers if this happens.</h2>
-	)
-}
---
+++ source/frontend/pages/404.tsx
@@ -0,0 +1,17 @@
+export default function Custom404() {
+	return (
+		<main className="content">
+			<header className="warning">
+				<div className="container">
+					<h1>404 - Page Not Found</h1>
+				</div>
+			</header>
+			<section>
+				<div className="container">
+					<p>The page you are trying to reach may be deleted, misspelled, or private.</p>
+					<p>If a link within our app led you to this page, please contact the developers.</p>
+				</div>
+			</section>
+		</main>
+	);
+}
