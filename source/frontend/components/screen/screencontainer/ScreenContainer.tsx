f31d97ac70e94966a1b928d1043af57e0c6cab6d
Matthew Crossman <matt@crossman.page>
2022-08-18 03:33:25 +0000

    Screen: add ScreenContainer prop type & docs

--- source/frontend/components/screen/screencontainer/ScreenContainer.tsx
+++ source/frontend/components/screen/screencontainer/ScreenContainer.tsx
@@ -15,44 +15,70 @@ import Panel from '../panel/panel';
  * Screen container props.
  */
 type ScreenContainerProps = {
-    screenOn?: boolean;
-    mediaOn?: boolean;
-    mediaSelectorOn?: boolean
-}
+	/**
+	 * Whether the screen initially starts on
+	 */
+	screenOn?: boolean;
+	/**
+	 * Whether the media is marked as initially on
+	 */
+	mediaOn?: boolean;
+	/**
+	 * Whether the media selector is marked initially as on
+	 */
+	mediaSelectorOn?: boolean;
+};
 
-class ScreenContainer extends Component<ScreenContainerProps> {
-    constructor(props: ScreenContainerProps) {
-        super(props);
-        this.state = {
-            screenOn: this.props.screenOn === undefined ? false : this.props.screenOn,
-            mediaOn: this.props.mediaOn === undefined ? false : this.props.mediaOn,
-            mediaSelectorOn: this.props.mediaSelectorOn === undefined ? false : this.props.mediaSelectorOn
-        }
-    } 
-    
-    toggleScreen = () => {
-        this.setState({screenOn: !this.state.screenOn})
-        if (this.state.screenOn){
-            this.setState({mediaSelectorOn: false}) // Screen selector shouldn't exist if the screen doesn't exist.
-        }
-    }
+/**
+ * Defines the state for the screen container.
+ *
+ * Can be defined inline but this is cleaner.
+ */
+type ScreenContainerState = {
+	/**
+	 * Screen activity state
+	 */
+	screenOn: boolean;
+	/**
+	 * Whether there is currently active media
+	 */
+	mediaOn: boolean;
+	/**
+	 * Whether the media selector is open
+	 */
+	mediaSelectorOn: boolean;
+};
 
-    toggleMedia = () => {
-        this.setState({mediaOn: !this.state.mediaOn})
-    }
+class ScreenContainer extends Component<ScreenContainerProps, ScreenContainerState> {
+	constructor(props: ScreenContainerProps) {
+		super(props);
+		this.state = {
+			screenOn: this.props.screenOn === undefined ? false : this.props.screenOn,
+			mediaOn: this.props.mediaOn === undefined ? false : this.props.mediaOn,
+			mediaSelectorOn:
+				this.props.mediaSelectorOn === undefined ? false : this.props.mediaSelectorOn,
+		};
+	}
 
-    toggleMediaSelector = () => {
-        this.setState({mediaSelectorOn: !this.state.mediaSelectorOn})
-    }
+	toggleScreen = () => {
+		this.setState({ screenOn: !this.state.screenOn });
+		if (this.state.screenOn) {
+			this.setState({ mediaSelectorOn: false }); // Screen selector shouldn't exist if the screen doesn't exist.
+		}
+	};
 
-    render() {
-        return (
-            <div
-                className={
-                    styles.container
-                }
-            >
-                <Card>
+	toggleMedia = () => {
+		this.setState({ mediaOn: !this.state.mediaOn });
+	};
+
+	toggleMediaSelector = () => {
+		this.setState({ mediaSelectorOn: !this.state.mediaSelectorOn });
+	};
+
+	render() {
+		return (
+			<div className={styles.container}>
+				<Card>
 					<h1>Component test</h1>
 
 					<p>This is a paragraph within a card.</p>
@@ -61,34 +87,29 @@ class ScreenContainer extends Component<ScreenContainerProps> {
 
 					<ButtonSet>
 						<Button onClick={this.toggleScreen}>
-                            <span>
-                                {this.state.screenOn ? "Hide screen" : "Show screen"}
-                            </span>
-                        </Button>
-                        {this.state.screenOn ? 
-                        <Button onClick={this.toggleMediaSelector}>
-                            <span>
-                                {"Change screen medium"}
-                            </span>
-                        </Button>
-                        : null}
+							<span>{this.state.screenOn ? 'Hide screen' : 'Show screen'}</span>
+						</Button>
+						{this.state.screenOn ? (
+							<Button onClick={this.toggleMediaSelector}>
+								<span>{'Change screen medium'}</span>
+							</Button>
+						) : null}
 					</ButtonSet>
 				</Card>
-				{this.state.screenOn ? <Panel label="Screen" media={this.state.mediaOn} expandable/> : null}
-                {this.state.mediaSelectorOn ? 
-                <Card>
-                        <Panel label="Screen Example" media={false}/>
-                        <Button onClick={this.toggleMedia}>
-                            <span>
-                                {"Toggle media"}
-                            </span>
-                        </Button>
-                </Card> 
-                : null}
-
-            </div>
-        );
-    }
+				{this.state.screenOn ? (
+					<Panel label="Screen" media={this.state.mediaOn} expandable />
+				) : null}
+				{this.state.mediaSelectorOn ? (
+					<Card>
+						<Panel label="Screen Example" media={false} />
+						<Button onClick={this.toggleMedia}>
+							<span>{'Toggle media'}</span>
+						</Button>
+					</Card>
+				) : null}
+			</div>
+		);
+	}
 }
- 
+
 export default ScreenContainer;
