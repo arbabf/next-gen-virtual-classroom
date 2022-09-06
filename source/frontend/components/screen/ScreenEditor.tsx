38219c9b039c26e4364b911b51455fafdb4799d9
Matthew Crossman <matt@crossman.page>
2022-09-06 00:59:17 +0000

    Screen: Add docstring to editor

--- source/frontend/components/screen/ScreenEditor.tsx
+++ source/frontend/components/screen/ScreenEditor.tsx
@@ -1,6 +1,9 @@
 import { Component, PropsWithChildren } from 'react';
 import styles from './ScreenEditor.module.css';
 
+/**
+ * Displays all of the editing/settings of a screen within the display/panel space.
+ */
 export class ScreenEditor extends Component<PropsWithChildren> {
 	render() {
 		return <div className={styles.editor}>{this.props.children}</div>;
