525f3d7862bef9b754c7771320e1707f6c193207
Matthew Crossman <matt@crossman.page>
2022-09-19 15:25:19 +1000

    Badge: clean up how sets are styled

--- source/frontend/components/common/badge/BadgeSet.tsx
+++ source/frontend/components/common/badge/BadgeSet.tsx
@@ -1,9 +1,15 @@
 import { Component, PropsWithChildren } from "react";
 import styles from "./BadgeSet.module.css";
 
-export class BadgeSet extends Component<PropsWithChildren> {
+type BadgeSetProps = {
+	className?: string;
+}
+
+export class BadgeSet extends Component<PropsWithChildren<BadgeSetProps>> {
 	render() {
 		let classes: string = styles.set;
+
+		if (this.props.className) classes += ' ' + this.props.className;
 
 		return <div className={classes}>
 			{this.props.children}
