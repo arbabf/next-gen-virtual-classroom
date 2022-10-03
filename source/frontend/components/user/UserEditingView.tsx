c61d4984be6412112f370723c0675ac2906199ed
Matthew Crossman <matt@crossman.page>
2022-10-03 20:40:17 +1100

    UserEditingView: format & sort imports

--- source/frontend/components/user/UserEditingView.tsx
+++ source/frontend/components/user/UserEditingView.tsx
@@ -5,9 +5,8 @@ import { AvatarView } from '../avatars/AvatarView';
 import { Badge } from '../common/badge/Badge';
 import { BadgeSet } from '../common/badge/BadgeSet';
 import { BadgeUserTypes } from '../common/badge/BadgeTypes';
+import Button from '../common/button/button';
 import styles from './UserEditingView.module.css';
-import Button from '../common/button/button'
-import ReactDOM from 'react-dom';
 
 type UserEditingViewProps = {
 	/**
@@ -22,11 +21,11 @@ type UserEditingViewState = {
 	 */
 	showContextMenu: boolean;
 
-    newName: string;
+	newName: string;
 
-    newID: string;
+	newID: string;
 
-    newEmail: string|undefined;
+	newEmail: string | undefined;
 }
 
 
@@ -35,14 +34,14 @@ export class UserEditingView extends Component<UserEditingViewProps, UserEditing
 
 	state: UserEditingViewState = {
 		showContextMenu: false,
-        newName: this.props.user.globalInfo.name,
-        newID: this.props.user.globalInfo.id,
-        newEmail: this.props.user.globalInfo.email
+		newName: this.props.user.globalInfo.name,
+		newID: this.props.user.globalInfo.id,
+		newEmail: this.props.user.globalInfo.email
 	}
 
-    render(){
+	render() {
 
-        assert(this.props.user instanceof RoomUser);
+		assert(this.props.user instanceof RoomUser);
 		let avatar = this.props.user.getAvatar();
 
 		// get user's avatar aspect ratio
@@ -51,73 +50,73 @@ export class UserEditingView extends Component<UserEditingViewProps, UserEditing
 		let classes = styles.frame;
 		let badgeSetClasses = styles.badgeSet;
 
-        let roleBadgeType: BadgeUserTypes | undefined;
+		let roleBadgeType: BadgeUserTypes | undefined;
 
-        return(
+		return (
 			<div className={styles.container}>
-                <span className={styles.namePreview}>{this.props.user.globalInfo.name}</span>
-                <span className={styles.namePreview}>{this.props.user.globalInfo.email}</span>
-                <span className={styles.namePreview}>{this.props.user.globalInfo.id}</span>
-                <span>
-                    <div className={styles.badgeWrapper}>
-                        <BadgeSet className={badgeSetClasses}>
-                            {roleBadgeType && <Badge type={roleBadgeType} />}
-                            {<Badge type={BadgeUserTypes.you} />}
-                        </BadgeSet>
-                        <div className={classes}>
-                            <AvatarView avatar={avatar} />
-                        </div>
-                        <BadgeSet className={badgeSetClasses}>
-                            {this.props.user.state?.currentActions.map((action) => <Badge key={action} type={action} />)}
-                        </BadgeSet>
-                    </div>
-                </span>
+				<span className={styles.namePreview}>{this.props.user.globalInfo.name}</span>
+				<span className={styles.namePreview}>{this.props.user.globalInfo.email}</span>
+				<span className={styles.namePreview}>{this.props.user.globalInfo.id}</span>
+				<span>
+					<div className={styles.badgeWrapper}>
+						<BadgeSet className={badgeSetClasses}>
+							{roleBadgeType && <Badge type={roleBadgeType} />}
+							{<Badge type={BadgeUserTypes.you} />}
+						</BadgeSet>
+						<div className={classes}>
+							<AvatarView avatar={avatar} />
+						</div>
+						<BadgeSet className={badgeSetClasses}>
+							{this.props.user.state?.currentActions.map((action) => <Badge key={action} type={action} />)}
+						</BadgeSet>
+					</div>
+				</span>
 
 
-                <span className={styles.name}>Name</span>
-                <textarea id = "textName" value={this.state.newName} onChange={this.onNameChange.bind(this)}/>
-                <span className={styles.name}>Email</span>
-                <textarea id = "textEmail" value={this.state.newEmail} onChange={this.onEmailChange.bind(this)}/>
-                <span className={styles.name}>ID</span>
-                <textarea id = "textID" value={this.props.user.globalInfo.id} onChange={this.onIDChange.bind(this)}/>
-                
-                <Button onClick = {this.onSaveClick.bind(this)}>
-                    Save
-                </Button>
-                <div id = "Saved">
+				<span className={styles.name}>Name</span>
+				<textarea id="textName" value={this.state.newName} onChange={this.onNameChange.bind(this)} />
+				<span className={styles.name}>Email</span>
+				<textarea id="textEmail" value={this.state.newEmail} onChange={this.onEmailChange.bind(this)} />
+				<span className={styles.name}>ID</span>
+				<textarea id="textID" value={this.props.user.globalInfo.id} onChange={this.onIDChange.bind(this)} />
 
-                </div>
+				<Button onClick={this.onSaveClick.bind(this)}>
+					Save
+				</Button>
+				<div id="Saved">
+
+				</div>
 			</div>
-            )
-        }
-        private onNameChange(event: React.ChangeEvent<HTMLTextAreaElement>){
-            this.setState({newName: event.target.value})
-        }
+		)
+	}
+	private onNameChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+		this.setState({ newName: event.target.value })
+	}
 
-        private onIDChange(event: React.ChangeEvent<HTMLTextAreaElement>){
-            this.setState({newID: event.target.value})
-        }
+	private onIDChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+		this.setState({ newID: event.target.value })
+	}
 
-        private onEmailChange(event: React.ChangeEvent<HTMLTextAreaElement>){
-            this.setState({newEmail: event.target.value})
-        }
+	private onEmailChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+		this.setState({ newEmail: event.target.value })
+	}
 
-        private onSaveClick(this: this){
-            if(this.state.newID !== this.props.user.globalInfo.id){
+	private onSaveClick(this: this) {
+		if (this.state.newID !== this.props.user.globalInfo.id) {
 
-            }
-            if(this.state.newName !== this.props.user.globalInfo.name){
-                this.props.user.globalInfo.name = this.state.newName
-            }
+		}
+		if (this.state.newName !== this.props.user.globalInfo.name) {
+			this.props.user.globalInfo.name = this.state.newName
+		}
 
-            if(this.state.newEmail !== this.props.user.globalInfo.email){
-                this.props.user.globalInfo.email = this.state.newEmail
-            }
+		if (this.state.newEmail !== this.props.user.globalInfo.email) {
+			this.props.user.globalInfo.email = this.state.newEmail
+		}
 
-            if(document.getElementById('Saved')){
-                document.getElementById('Saved').innerHTML = "Saved"
-            }
-        }
+		if (document.getElementById('Saved')) {
+			document.getElementById('Saved').innerHTML = "Saved"
+		}
+	}
 
 
 
